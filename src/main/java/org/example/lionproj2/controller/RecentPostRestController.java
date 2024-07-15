abd11d1f9da86745bda85ed815e27996b01bea6f
Sechang Jang <sehan528@gmail.com>
2024-07-15 20:42:32 +0900

    refactor: merge RecentPostRestController

--- src/main/java/org/example/lionproj2/controller/RecentPostRestController.java
+++ src/main/java/org/example/lionproj2/controller/RecentPostRestController.java
@@ -3,6 +3,7 @@ package org.example.lionproj2.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.lionproj2.dto.RecentPostDTO;
 import org.example.lionproj2.service.RecentPostService;
+import org.example.lionproj2.service.RecentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +15,36 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/recent")
 @RequiredArgsConstructor
+
+//public class RecentPostRestController {
+//    private final RecentPostService recentPostService;
+//
+//    @GetMapping
+//    public ResponseEntity<List<RecentPostDTO>> getRecentPosts(
+//            @RequestParam Long userId,
+//            @RequestParam(defaultValue = "0") int page,
+//            @RequestParam(defaultValue = "20") int size) {
+//        List<RecentPostDTO> recentPosts = recentPostService.getRecentPosts(userId, page, size);
+//        return ResponseEntity.ok(recentPosts);
+//    }
+//}
 public class RecentPostRestController {
     private final RecentPostService recentPostService;
+    private final RecentService recentService;
 
     @GetMapping
     public ResponseEntity<List<RecentPostDTO>> getRecentPosts(
-            @RequestParam Long userId,
+            @RequestParam(required = false) Long userId,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int size) {
-        List<RecentPostDTO> recentPosts = recentPostService.getRecentPosts(userId, page, size);
+        List<RecentPostDTO> recentPosts;
+
+        // userId Check. == is it login?
+        if (userId != null) {
+            recentPosts = recentPostService.getRecentPosts(userId, page, size);
+        } else {
+            recentPosts = recentService.getRecentPosts(page, size);
+        }
         return ResponseEntity.ok(recentPosts);
     }
 }
